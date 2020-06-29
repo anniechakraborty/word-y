c37ec227763e9aa54b7fd288bba1a8d7acd02f21
Annie Chakraborty <the.demented.genius29@gmail.com>
2020-06-30 00:25:35 +0530

    Started building

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData.dark().copyWith(
+        accentColor: Colors.purple[900],
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Word-Y',
+        ),
+      ),
+      body: Container(),
+    );
+  }
+}
