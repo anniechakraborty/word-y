bb83e6f687721d3972838da34408b771b5bbfd33
Annie Chakraborty <the.demented.genius29@gmail.com>
2020-07-01 21:37:31 +0530

    On tapping a word pair, it is being saved in the savedWordPair set. Also made separate files to store the different classes and changed app theme.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+
+import 'random_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,57 +9,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage(),
-      theme: ThemeData.dark().copyWith(
-        accentColor: Colors.purple[900],
+      theme: ThemeData(
+        primaryColor: Colors.teal[900],
+        scaffoldBackgroundColor: Colors.teal[700],
       ),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final _randomWordPair = <WordPair>[];
-  Widget _buildWordList() {
-    return ListView.builder(
-      padding: EdgeInsets.all(16.0),
-      itemBuilder: (context, items) {
-        if (items.isOdd) {
-          return Divider();
-        }
-        final index = items ~/ 2;
-        if (index >= _randomWordPair.length) {
-          _randomWordPair.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_randomWordPair[index]);
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: TextStyle(
-          fontSize: 15.0,
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Word-Y',
-        ),
-      ),
-      body: _buildWordList(),
+      title: 'Word - Y',
+      debugShowCheckedModeBanner: false,
     );
   }
 }
---
+++ lib/random_words.dart
@@ -0,0 +1,74 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _randomWordPair = <WordPair>[];
+  final _savedWordPairs = Set<WordPair>();
+  Widget _buildWordList() {
+    return ListView.builder(
+      padding: EdgeInsets.all(16.0),
+      itemBuilder: (context, items) {
+        if (items.isOdd) {
+          return Divider();
+        }
+        final index = items ~/ 2;
+        if (index >= _randomWordPair.length) {
+          _randomWordPair.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_randomWordPair[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _savedWordPairs.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: TextStyle(
+          fontSize: 18.0,
+          color: Colors.white,
+          wordSpacing: 2.0,
+          fontStyle: FontStyle.italic,
+        ),
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.redAccent : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _savedWordPairs.remove(pair);
+          } else {
+            _savedWordPairs.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  void _pushSaved() {}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Word-Y',
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.playlist_add),
+            onPressed: _pushSaved,
+          ),
+        ],
+      ),
+      body: _buildWordList(),
+    );
+  }
+}
