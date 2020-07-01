95810fb7abf98b4932d1f82749cb8eedd37b5f37
Annie Chakraborty <the.demented.genius29@gmail.com>
2020-07-01 21:55:46 +0530

    A new screen shows all the saved word pairs.

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -1,6 +1,13 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
+const textStyle = TextStyle(
+  fontSize: 18.0,
+  color: Colors.white,
+  wordSpacing: 2.0,
+  fontStyle: FontStyle.italic,
+);
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -30,12 +37,7 @@ class _HomePageState extends State<HomePage> {
     return ListTile(
       title: Text(
         pair.asPascalCase,
-        style: TextStyle(
-          fontSize: 18.0,
-          color: Colors.white,
-          wordSpacing: 2.0,
-          fontStyle: FontStyle.italic,
-        ),
+        style: textStyle,
       ),
       trailing: Icon(
         alreadySaved ? Icons.favorite : Icons.favorite_border,
@@ -53,7 +55,32 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  void _pushSaved() {}
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _savedWordPairs.map((WordPair pair) {
+        return ListTile(
+          title: Text(
+            pair.asPascalCase,
+            style: textStyle,
+          ),
+        );
+      });
+      final List<Widget> _savedList = ListTile.divideTiles(
+        tiles: tiles,
+        context: context,
+      ).toList();
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Saved WordPairs'),
+        ),
+        body: ListView(
+          children: _savedList,
+        ),
+      );
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
